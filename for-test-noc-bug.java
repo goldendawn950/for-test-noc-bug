c1977b21ebbca345b808fcf32b87aa88204991e3
goldendawn950 <139677238+goldendawn950@users.noreply.github.com>
2023-11-02 01:25:44 +0100

    Update for-test-noc-bug.java

--- for-test-noc-bug.java
+++ for-test-noc-bug.java
@@ -1,1 +1,11 @@
-for-test-noc-bug
+public final class Range<C extends Comparable> extends RangeGwtSerializationDependencies
+    implements Predicate<C>, Serializable {
+
+  static class LowerBoundFn implements Function<Range, Cut> {
+    static final LowerBoundFn INSTANCE = new LowerBoundFn();
+
+    @Override
+    public Cut apply(Range range) {
+      return range.lowerBound;
+    }
+  }
